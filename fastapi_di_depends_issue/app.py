a4ad7a873f2598b2ce200b13614f8f3b15a57cb2
Max Froehlich <max.froehlich@serviceware.de>
2021-07-13 13:57:51 +0200

    showcase workarround

--- fastapi_di_depends_issue/app.py
+++ fastapi_di_depends_issue/app.py
@@ -1,8 +1,4 @@
-
-
-
-from fastapi.applications import FastAPI
-from fastapi.param_functions import Depends
+from fastapi import FastAPI, Depends, Request
 from fastapi.security.http import HTTPBasic, HTTPBasicCredentials
 from dependency_injector.wiring import Provide, inject
 from fastapi_di_depends_issue.container import Container
@@ -11,18 +7,35 @@ app = FastAPI()
 
 #### working
 fixed_bearer = HTTPBasic()
+
+
 @app.get("/fixed")
 def fixed(user: HTTPBasicCredentials = Depends(fixed_bearer)):
 
-    return {"username" : user.username}
+    return {"username": user.username}
+
 
 #### not working
 di_bearer = Provide[Container.bearer]
+
 
 @app.get("/with_di")
 @inject
 def with_di(user: HTTPBasicCredentials = Depends(di_bearer)):
 
-    return {"username" : user.username if user else None}
+    return {"username": user.username if user else None}
 
 
+@inject
+async def workarround(
+    request: Request, di_bearer=Provide[Container.bearer]
+) -> HTTPBasicCredentials:
+
+    return await di_bearer(request)
+
+
+@app.get("/with_di_workarround")
+@inject
+def with_di_workarround(user: HTTPBasicCredentials = Depends(workarround)):
+
+    return {"username": user.username if user else None}
--- tests/test_fastapi_di_depends_issue.py
+++ tests/test_fastapi_di_depends_issue.py
@@ -1,37 +1,43 @@
-
 from fastapi_di_depends_issue import app
 from fastapi_di_depends_issue.container import Container
 from fastapi.testclient import TestClient
 import pytest
 import base64
+
 container = Container()
 
 
 @pytest.fixture()
-def client()-> TestClient:
+def client() -> TestClient:
     container.wire(modules=[app])
     return TestClient(app.app)
-    
 
-def auth(username:str, password: str) -> str:
-    plain = f"{username}:{password}".encode('ascii')
-    encoded = base64.b64encode(plain).decode('ascii')
+
+def auth(username: str, password: str) -> str:
+    plain = f"{username}:{password}".encode("ascii")
+    encoded = base64.b64encode(plain).decode("ascii")
     return f"Basic {encoded}"
 
+
 def test_fixed(client: TestClient):
-    
-    resp = client.get("/fixed", headers={
-        "Authorization": auth("foo","foo")
-    })
+
+    resp = client.get("/fixed", headers={"Authorization": auth("foo", "foo")})
     resp.raise_for_status()
-    
+
     assert resp.json()["username"] == "foo"
 
 
 def test_di(client: TestClient):
 
-    resp = client.get("/with_di", headers={
-         "Authorization": auth("bar","bar")
-    })
+    resp = client.get("/with_di", headers={"Authorization": auth("bar", "bar")})
+    resp.raise_for_status()
+    assert resp.json()["username"] == "bar"
+
+
+def test_di_workarround(client: TestClient):
+
+    resp = client.get(
+        "/with_di_workarround", headers={"Authorization": auth("bar", "bar")}
+    )
     resp.raise_for_status()
     assert resp.json()["username"] == "bar"
